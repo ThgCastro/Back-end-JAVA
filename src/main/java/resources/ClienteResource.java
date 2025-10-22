aadde4af30d7e5ebd296882a5ea7e17983cf9973
Thiago <nascimentocastro989@gmail.com>
2025-10-22 17:59:04 -0300

    create resources

---
+++ src/main/java/resources/ClienteResource.java
@@ -0,0 +1,31 @@
+package resources;
+
+import com.unip.crud.model.Cliente;
+import com.unip.crud.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+    @Autowired
+    private ClienteService service;
+
+    public ResponseEntity<List<Cliente>> findAll(){
+        List<Cliente> list = service.saveCliente();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable Long id){
+        Cliente obj = service.;
+        return ResponseEntity.ok().body(obj);
+    }
+}
